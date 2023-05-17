a076b5d2ebfd88b38c68427e511f5ae012e21e0f
Danztee <olowoniyidaniel@gmail.com>
2023-05-17 22:15:16 +0100

    update

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -10,10 +10,11 @@ const CustomLink: React.FC<CustomLinkProps> = ({ value, outline }) => {
   return (
     <Link
       href="/"
-      className={`py-[10px] px-[25px] rounded-full  hover:opacity-90
+      className={`px-[25px] rounded-full  hover:opacity-90
       ${outline ? "bg-[transparent]" : "bg-[#18753D]"}
       ${outline ? "border border-[#18753D]" : ""}
       ${outline ? "text-[#505050]" : "text-[#fff]"}
+      ${value === "Sign In" || value === "Sign Up" ? "py-[10px]" : "py-[15px]"}
       `}
     >
       {value}
