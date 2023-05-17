22afb451a22d9b777e0296d6a443a65402e56bbb
Danztee <olowoniyidaniel@gmail.com>
2023-05-17 21:49:16 +0100

    fix: imports

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,37 +1,9 @@
-// import Link from "next/link";
-// import React from "react";
-// import CustomLink from "../CustomLink";
 import Image from "next/image";
-
 import { useState } from "react";
-import NavLinks from "../NavLinks";
-
-import logo from "../../../public/logo.png";
-
 import { RxHamburgerMenu } from "react-icons/rx";
+import logo from "../../../public/logo.png";
 import CustomLink from "../CustomLink";
-// import NavLinks from "../NavLinks";
-
-// type NavbarProps = {};
-
-// const Navbar: React.FC<NavbarProps> = () => {
-//   return (
-//     <>
-//       <nav className="flex justify-between lg:items-center px-4 lg:px-8 py-4 flex-col items-start gap-4">
-//         <div>
-//           <Image src={logo} alt="logo" width={120} height={120} />
-//         </div>
-
-//         <NavLinks />
-
-//         <div className="lg:hidden">
-//           <RxHamburgerMenu size={30} />
-//         </div>
-//       </nav>
-//     </>
-//   );
-// };
-// export default Navbar;
+import NavLinks from "../NavLinks";
 
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
